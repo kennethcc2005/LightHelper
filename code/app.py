8754236796905a9983f9832be4e825fcf581ddca
Zoesh <zoesh@Zoeshs-MacBook-Pro.local>
2016-04-18 21:46:51 -0700

    flask clone

---
+++ code/app.py
@@ -0,0 +1,13 @@
+from flask import Flask, request, render_template
+import os
+app = Flask(__name__)
+
+# home page
+@app.route('/')
+def index():
+
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
