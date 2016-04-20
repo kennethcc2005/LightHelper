d57dd231594403419454ce38c6596bf8d585571b
Xi Huang <dayumikoda@gmail.com>
2016-04-20 02:13:33 -0700

    setup alembic for db init; db migrate; and db upgrade

--- code/app.py
+++ code/app.py
@@ -1,13 +1,22 @@
 from flask import Flask, request, render_template
-import os
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
+
 app = Flask(__name__)
+app.config.from_object('config')
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
 
 # home page
 @app.route('/')
 def index():
 
     return render_template('index.html')
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
---
+++ code/models.py
@@ -0,0 +1,8 @@
+from app import db, manager
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128))
+
+if __name__ == '__main__':
+    manager.run()
